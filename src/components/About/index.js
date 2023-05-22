1658bcefd5987ead589f0930fda45936d1ecac63
Esteban Manrupe <esteban_manrupe@hotmail.com>
2023-05-21 23:58:03 -0300

    fix some styles

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -24,7 +24,7 @@ const About = () => {
   return (
     <>
       <div className="container about-page">
-        <div className="text-zone">
+        <div className="text-zone text-zone-narrow">
           <h1>
             <AnimatedLetters
               letterClass={letterClass}
